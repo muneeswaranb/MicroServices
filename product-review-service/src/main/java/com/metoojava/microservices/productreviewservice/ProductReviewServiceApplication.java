cbec7196501abc512bb29769ab20671b9e48085b
MBalasu2 <muneeswaran.balasubramanian2@t-mobile.com>
2019-04-25 08:36:11 -0700

    Product review service: Initial version

---
+++ product-review-service/src/main/java/com/metoojava/microservices/productreviewservice/ProductReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.metoojava.microservices.productreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReviewServiceApplication.class, args);
+	}
+
+}
