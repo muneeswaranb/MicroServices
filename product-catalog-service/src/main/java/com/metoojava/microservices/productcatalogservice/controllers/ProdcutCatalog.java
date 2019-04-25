525449c0ca4d3adb9adb78b4da68bd118b535129
MBalasu2 <muneeswaran.balasubramanian2@t-mobile.com>
2019-04-25 08:30:06 -0700

    Added a method to retrieve details using Path variable.

--- product-catalog-service/src/main/java/com/metoojava/microservices/productcatalogservice/controllers/ProdcutCatalog.java
+++ product-catalog-service/src/main/java/com/metoojava/microservices/productcatalogservice/controllers/ProdcutCatalog.java
@@ -3,6 +3,7 @@ package com.metoojava.microservices.productcatalogservice.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.metoojava.microservices.productcatalogservice.model.Item;
 
 @RestController
-@RequestMapping(path="/catalog")
+@RequestMapping(path = "/catalog")
 public class ProdcutCatalog {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Item> getCatalog(){
-		List<Item> catalog=new ArrayList<>();
-		Item samsungS8 =new Item("S8","Samsing Galaxy S8",799.00);
-		Item iphone10 =new Item("i10","Apple Iphone X",999.00);
+	List<Item> catalog = new ArrayList<>();
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Item> getCatalog() {
+		Item samsungS8 = new Item("S8", "Samsing Galaxy S8", 799.00);
+		Item iphone10 = new Item("i10", "Apple Iphone X", 999.00);
 		catalog.add(samsungS8);
 		catalog.add(iphone10);
-		return catalog;		
+		return catalog;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
+	public Item getPhoneDetails(@PathVariable("id") String id) {
+		Item itemDetails = null;
+		for (Item item : catalog) {
+			if (id.equalsIgnoreCase(item.getProductId())) {
+				itemDetails = item;
+			}
+		}
+		return itemDetails;
 	}
 }
